2fecd4ebf4679d412f7eddf8048521beab2195b3
jumpdart <markhoward000@gmail.com>
2020-12-04 13:46:30 -0500

    basic nav and debug profiles

--- App.tsx
+++ App.tsx
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function TrackHistoryScreen() {
+  return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>HistoryScreen</Text>
+      </View>
+  );
+}
+function CurrentTrackScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Details Screen xx</Text>
+        </View>
+    );
+}
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <>
+          <NavigationContainer>
+              <Tab.Navigator>
+                  <Tab.Screen name="History" component={TrackHistoryScreen} />
+                  <Tab.Screen name="Current" component={CurrentTrackScreen} />
+              </Tab.Navigator>
+          </NavigationContainer>
+      </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
